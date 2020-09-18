ef61184fa3d38eb379fe91eb97d08796c5e07a30
Christopher Bernjus <christopher.bernjus@stud.tu-darmstadt.de>
2020-09-18 03:16:11 +0200

    fix: renumber assignment to #3

--- assignments/03_if_statements/statements.py
+++ assignments/03_if_statements/statements.py
@@ -1,4 +1,4 @@
-# Homework Assignment  # 2 - IF Statements
+# Homework Assignment # 3 - IF Statements
 
 def anyEqual(first, second, third):
     return (int(first) == int(second) | int(second) == int(third) | int(first) == int(third))
