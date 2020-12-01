21415609e85b628a1544f0d33ec057b8d095c168
Christopher Bernjus <christopher.bernjus@stud.tu-darmstadt.de>
2020-12-01 01:08:49 +0100

    chore(lectures_11): rename blackjack to lonelyjack

---
+++ lectures/11_blackjack/lonelyjack.py
@@ -0,0 +1,35 @@
+from deck import Deck
+from player import Player
+
+
+def printLogo():
+    print("""  _                     _           _            _    
+ | |    ___  _ __   ___| |_   _    | | __ _  ___| | __
+ | |   / _ \\| '_ \\ / _ \\ | | | |_  | |/ _` |/ __| |/ /
+ | |__| (_) | | | |  __/ | |_| | |_| | (_| | (__|   < 
+ |_____\\___/|_| |_|\\___|_|\\__, |\\___/ \\__,_|\\___|_|\\_\\
+ ------------------------ |___/ ----------------------
+    """)
+
+
+def drawCards(deck, number):
+    return [deck.cards.pop() for _ in range(number)]
+
+
+def printHouse(house_hand):
+    printArr = []
+    for card in house_hand:
+        printArr.append(str(card))
+    printArr[0] = "XX"
+    print("House: " + ' '.join(printArr))
+
+
+printLogo()
+cardDeck = Deck()
+cardDeck.random()
+
+playerOne = Player(drawCards(cardDeck, 2))
+house = Player(drawCards(cardDeck, 2))
+
+print(playerOne)
+printHouse(house.hand)
